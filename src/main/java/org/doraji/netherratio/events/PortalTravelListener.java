4bc7a703e4d4139fa2b17bf21a7dad64ac8879aa
NEK Nether Ninja <onlinechessllc@gmail.com>
2026-06-16 19:14:27 -0400

    Update PortalTravelListener.java

--- src/main/java/org/doraji/netherratio/events/PortalTravelListener.java
+++ src/main/java/org/doraji/netherratio/events/PortalTravelListener.java
@@ -4,12 +4,13 @@ import org.doraji.netherratio.NetherRatio;
 import org.doraji.netherratio.ConfigManager;
 import org.doraji.netherratio.util.CoordinateMath;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.World;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerPortalEvent;
-import org.bukkit.event.player.PlayerTeleportEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PortalTravelListener implements Listener {
 
@@ -19,54 +20,61 @@ public class PortalTravelListener implements Listener {
     public PortalTravelListener(NetherRatio plugin) {
         this.plugin = plugin;
         this.cm = plugin.getConfigManager();
-        plugin.getLogger().info("§a[NetherRatio] Listener - Vanilla portal creation mode");
+        plugin.getLogger().info("§a[NetherRatio TRACE] Listener loaded - Pure tracing mode");
     }
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
-    public void onPlayerPortal(PlayerPortalEvent event) {
-        if (event.getCause() != PlayerTeleportEvent.TeleportCause.NETHER_PORTAL) return;
+    public void onPlayerMove(PlayerMoveEvent event) {
+        Player player = event.getPlayer();
+        Location to = event.getTo();
+        if (to == null) return;
 
-        plugin.getLogger().info("§e[NetherRatio] PlayerPortalEvent caught for " + event.getPlayer().getName());
+        if (to.getBlockX() == event.getFrom().getBlockX() &&
+            to.getBlockY() == event.getFrom().getBlockY() &&
+            to.getBlockZ() == event.getFrom().getBlockZ()) return;
 
-        Location newTo = calculateDestination(event.getFrom());
-        if (newTo != null) {
-            plugin.getLogger().info("§a[NetherRatio] Overriding destination to " + formatLoc(newTo));
-            event.setTo(newTo);
-            event.setCancelled(false);   // Let vanilla create the portal frame
-        }
+        if (to.getBlock().getType() != Material.NETHER_PORTAL) return;
+
+        plugin.getLogger().info("§e[NetherRatio TRACE] === PORTAL DETECTED for " + player.getName() + " ===");
+
+        // Trace the ratio lookup
+        World fromWorld = to.getWorld();
+        plugin.getLogger().info("§e[NetherRatio TRACE] From world: " + fromWorld.getName() + " | Environment: " + fromWorld.getEnvironment());
+
+        double ratio = (fromWorld.getEnvironment() == World.Environment.NORMAL) 
+            ? cm.getRatioForWorld(fromWorld.getName()) 
+            : cm.getRatioForNetherWorld(fromWorld.getName());
+
+        plugin.getLogger().info("§e[NetherRatio TRACE] Ratio retrieved from ConfigManager: " + ratio);
+
+        Location calculated = calculateDestination(to);
+        plugin.getLogger().info("§e[NetherRatio TRACE] Calculated destination: " + formatLoc(calculated));
     }
 
     private Location calculateDestination(Location from) {
         World fromWorld = from.getWorld();
         if (fromWorld == null) return null;
 
-        World toWorld;
-        double newX, newZ;
-        double scale;
+        World toWorld = null;
+        double newX = from.getX();
+        double newZ = from.getZ();
+        double scale = 8.0; // default
 
         if (fromWorld.getEnvironment() == World.Environment.NORMAL) {
             toWorld = cm.getLinkedNetherWorld(fromWorld.getName());
-            if (toWorld == null) return null;
             scale = cm.getRatioForWorld(fromWorld.getName());
             newX = CoordinateMath.toNether(from.getX(), scale, cm.getOffsetXForWorld(fromWorld.getName()));
             newZ = CoordinateMath.toNether(from.getZ(), scale, cm.getOffsetZForWorld(fromWorld.getName()));
         } else if (fromWorld.getEnvironment() == World.Environment.NETHER) {
             toWorld = cm.getLinkedOverworld(fromWorld.getName());
-            if (toWorld == null) return null;
             scale = cm.getRatioForNetherWorld(fromWorld.getName());
             newX = CoordinateMath.toOverworld(from.getX(), scale, cm.getOffsetXForNetherWorld(fromWorld.getName()));
             newZ = CoordinateMath.toOverworld(from.getZ(), scale, cm.getOffsetZForNetherWorld(fromWorld.getName()));
-        } else {
-            return null;
         }
 
-        if (cm.areBoundsEnabled() && !cm.areCoordinatesWithinBounds(newX, newZ)) {
-            double[] clamped = cm.clampCoordinates(newX, newZ);
-            newX = clamped[0];
-            newZ = clamped[1];
-        }
+        plugin.getLogger().info("§e[NetherRatio TRACE] Final ratio used: " + scale + " | New coords: " + newX + ", " + newZ);
 
-        return new Location(toWorld, newX + 0.5, 70, newZ + 0.5, from.getYaw(), from.getPitch());
+        return new Location(toWorld, newX, 80, newZ);
     }
 
     private String formatLoc(Location loc) {
